4c594b0521999119c93b153a42d7eab4c622e98b
eswaribala <Parameswaribala@gmail.com>
2018-06-06 15:32:36 +0530

    Add files via upload

---
+++ inheritance.ts
@@ -0,0 +1,7 @@
+import {Product} from "./oopsdemo";
+
+class SeasonalProduct extends Product{
+
+}
+var seasonalProduct=new SeasonalProduct(28345,"AC");
+console.log(seasonalProduct);
--- oopsdemo.ts
+++ oopsdemo.ts
@@ -1,4 +1,4 @@
-class Product
+export class Product
 {
     private productId:Number;
     private productName:String;
